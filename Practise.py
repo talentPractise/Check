95969cb9b75ff2ccf437bcc35af43d244bdd6be9
Talent <tcassistance103@gmail.com>
2025-08-26 21:12:51 +0530

    Update Practise.py

--- Practise.py
+++ Practise.py
@@ -1,4 +1,1 @@
-python -m pip install \
-  aiohttp pydantic fastapi uvicorn python-dotenv requests \
-  google-cloud-spanner sqlalchemy-spanner xmltodict \
-  pytest pytest-asyncio behave black isort flake8 pylint
+pip install aiohttp pydantic fastapi uvicorn python-dotenv requests google-cloud-spanner sqlalchemy-spanner xmltodict
