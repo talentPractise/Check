3c72da4fa73b1157c268366b6209f7ce267e6f34
Talent <tcassistance103@gmail.com>
2025-09-05 01:56:56 +0530

    Update Practise.py

--- Practise.py
+++ Practise.py
@@ -1,3 +1,4 @@
+N-N
 {
   "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
   "zipCode": "78114",
@@ -36,6 +37,51 @@
       "nationalNetworkParticipation": "",
       "providerTier": ""
     }
+
+
+
+N-Y
+{
+  "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
+  "zipCode": "93110",
+  "benefitProductType": "Medical",
+  "languageCode": "11",
+  "service": {
+    "code": "21325",
+    "type": "CPT4"
+    "description": "Adult Office visit Age 30-39",
+    "supportingService": {
+      "code": "470",
+      "type": "DRG"
+    },
+    "modifier": {
+      "modifierCode": "E1"
+    },
+    "diagnosisCode": "F33 40",
+    "placeOfService": {
+      "code": "24"
+    }
+  },
+  "providerInfo": [
+    {
+      "serviceLocation": "3474632",
+      "providerType": "AC",
+      "speciality": {
+        "code": ""
+      },
+      "taxIdentificationNumber": "956419205",
+      "taxIdQualifier": "",
+      "providerNetworks": {
+        "networkID": "08076"
+      },
+      "providerIdentificationNumber": "5364778",
+      "nationalProviderId": "0000000000",
+      "nationalNetworkParticipation": "",
+      "providerTier": ""
+    }
+  ]
+}
+
   ]
 }
 
