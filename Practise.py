e142b1dc26c08551d38f244e4777e366c894d1a9
Talent <tcassistance103@gmail.com>
2025-09-05 01:29:50 +0530

    Update Practise.py

--- Practise.py
+++ Practise.py
@@ -1,12 +1,10 @@
-n-Y-Y       Y-Y SAME RATE
-
 {
   "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
-  "zipCode": "95630",
+  "zipCode": "78114",
   "benefitProductType": "Medical",
   "languageCode": "11",
   "service": {
-    "code": "70010",
+    "code": "70490",
     "type": "CPT4"
     "description": "Adult Office visit Age 30-39",
     "supportingService": {
@@ -23,17 +21,17 @@ n-Y-Y       Y-Y SAME RATE
   },
   "providerInfo": [
     {
-      "serviceLocation": "243235",
+      "serviceLocation": "1242330",
       "providerType": "HO",
       "speciality": {
-        "code": "20104"
+        "code": ""
       },
-      "taxIdentificationNumber": "942761692",
+      "taxIdentificationNumber": "742019736",
       "taxIdQualifier": "// Not provided in your data",
       "providerNetworks": {
-        "networkID": "05144"
+        "networkID": "02100"
       },
-      "providerIdentificationNumber": "6150705",
+      "providerIdentificationNumber": "6549390",
       "nationalProviderId": "0000000000",
       "nationalNetworkParticipation": "// Not provided in your data",
       "providerTier": "// Not provided in your data"
@@ -42,46 +40,3 @@ n-Y-Y       Y-Y SAME RATE
 }
 
 
-
-N-N DIFFERENT RATE ONLY
-
-{
-  "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
-  "zipCode": "95630",
-  "benefitProductType": "Medical",
-  "languageCode": "11",
-  "service": {
-    "code": "44205",
-    "type": "CPT4",
-    "description": "Adult Office visit Age 30-39",
-    "supportingService": {
-      "code": "470",
-      "type": "DRG"
-    },
-    "modifier": {
-      "modifierCode": "E1"
-    },
-    "diagnosisCode": "F33 40",
-    "placeOfService": {
-      "code": "21"
-    }
-  },
-  "providerInfo": [
-    {
-      "serviceLocation": "243235",
-      "providerType": "HO",
-      "speciality": {
-        "code": ""
-      },
-      "taxIdentificationNumber": "942761692",
-      "taxIdQualifier": "",
-      "providerNetworks": {
-        "networkID": "05144"
-      },
-      "providerIdentificationNumber": "6150705",
-      "nationalProviderId": "0000000000",
-      "nationalNetworkParticipation": "",
-      "providerTier": ""
-    }
-  ]
-}
