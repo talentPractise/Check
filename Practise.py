f04d72f108eb2d18cd0713ec8bf1ec9dd99531b0
Talent <tcassistance103@gmail.com>
2025-09-05 00:04:05 +0530

    Update Practise.py

--- Practise.py
+++ Practise.py
@@ -1,24 +1,89 @@
--- Adjust project.dataset names below
-WITH rates AS (
-  SELECT
-    r.*,
-    ph.score
-  FROM project.dataset.CET_RATES r
-  JOIN project.dataset.PAYMENT_METHOD_HIERARCHY ph
-    ON ph.payment_method_cd = r.payment_method_cd
-)
-SELECT
-  *
-FROM rates
-QUALIFY
-  -- 1) Multiple payment methods present in the group
-  COUNT(DISTINCT payment_method_cd) OVER (
-    PARTITION BY service_cd, product_cd, contract_type, service_type_cd, place_of_service_cd
-  ) > 1
-  AND
-  -- 2) All payment methods in that group have the SAME score
-  COUNT(DISTINCT score) OVER (
-    PARTITION BY service_cd, product_cd, contract_type, service_type_cd, place_of_service_cd
-  ) = 1
--- Optional cap while exploring
-LIMIT 500;
+n-Y-Y       Y-Y SAME RATE
+
+{
+  "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
+  "zipCode": "95630",
+  "benefitProductType": "Medical",
+  "languageCode": "11",
+  "service": {
+    "code": "70010",
+    "type": "CPT4"
+    "description": "Adult Office visit Age 30-39",
+    "supportingService": {
+      "code": "470",
+      "type": "DRG"
+    },
+    "modifier": {
+      "modifierCode": "E1"
+    },
+    "diagnosisCode": "F33 40",
+    "placeOfService": {
+      "code": "22"
+    }
+  },
+  "providerInfo": [
+    {
+      "serviceLocation": "243235",
+      "providerType": "HO",
+      "speciality": {
+        "code": "20104"
+      },
+      "taxIdentificationNumber": "942761692",
+      "taxIdQualifier": "// Not provided in your data",
+      "providerNetworks": {
+        "networkID": "05144"
+      },
+      "providerIdentificationNumber": "6150705",
+      "nationalProviderId": "0000000000",
+      "nationalNetworkParticipation": "// Not provided in your data",
+      "providerTier": "// Not provided in your data"
+    }
+  ]
+}
+
+
+
+N-N DIFFERENT RATE ONLY
+
+{
+  "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
+  "zipCode": "50659",
+  "benefitProductType": "Medical",
+  "languageCode": "11",
+  "service": {
+    "code": "J7336",
+    "type": "HCPC"
+    "description": "Adult Office visit Age 30-39",
+    "supportingService": {
+      "code": "470",
+      "type": "DRG"
+    },
+    "modifier": {
+      "modifierCode": "E1"
+    },
+    "diagnosisCode": "F33 40",
+    "placeOfService": {
+      "code": "11"
+    }
+  },
+  "providerInfo": [
+    {
+      "serviceLocation": "7480123",
+      "providerType": "OP",
+      "speciality": {
+        "code": "20104"
+      },
+      "taxIdentificationNumber": "421211021",
+      "taxIdQualifier": "// Not provided in your data",
+      "providerNetworks": {
+        "networkID": "08845"
+      },
+      "providerIdentificationNumber": "7688505",
+      "nationalProviderId": "1043203565",
+      "nationalNetworkParticipation": "// Not provided in your data",
+      "providerTier": "// Not provided in your data"
+    }
+  ]
+}
+
+
