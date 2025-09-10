5d9d6e3b8a4c4e7d36049035bf7bfd2cc68b1f4f
Talent <tcassistance103@gmail.com>
2025-09-11 02:50:32 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,54 @@
+
+
+
+{
+  "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
+  "zipCode": "32003",
+  "benefitProductType": "Medical",
+  "languageCode": "11",
+  "service": {
+    "code": "G0547",
+    "type": "HCPC"
+    "description": "Adult Office visit Age 30-39",
+    "supportingService": {
+      "code": "470",
+      "type": "DRG"
+    },
+    "modifier": {
+      "modifierCode": "E1"
+    },
+    "diagnosisCode": "F33 40",
+    "placeOfService": {
+      "code": "11"
+    }
+  },
+  "providerInfo": [
+    {
+      "serviceLocation": "5574926",
+      "providerType": "SW",
+      "speciality": {
+        "code": ""
+      },
+      "taxIdentificationNumber": "455523028",
+      "taxIdQualifier": "",
+      "providerNetworks": {
+        "networkID": "01830"
+      },
+      "providerIdentificationNumber": "9416920",
+      "nationalProviderId": "1023389806",
+      "nationalNetworkParticipation": "",
+      "providerTier": ""
+    }
+  ]
+}
+
+
+
+
+
+
+
+
 {
   "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
   "zipCode": "64132",
