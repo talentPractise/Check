e89934279c334bc81c4f823fa1e75b0a6e262f07
Talent <tcassistance103@gmail.com>
2025-09-11 20:31:26 +0530

    Update newPayload.py

--- newPayload.py
+++ newPayload.py
@@ -1,5 +1,48 @@
 {
   "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
+  "zipCode": "08053",
+  "benefitProductType": "Medical",
+  "languageCode": "11",
+  "service": {
+    "code": "99213",
+    "type": "CPT4"
+    "description": "Adult Office visit Age 30-39",
+    "supportingService": {
+      "code": "470",
+      "type": "DRG"
+    },
+    "modifier": {
+      "modifierCode": "E1"
+    },
+    "diagnosisCode": "F33 40",
+    "placeOfService": {
+      "code": "11"
+    }
+  },
+  "providerInfo": [
+    {
+      "serviceLocation": "11895727226157",
+      "providerType": "",
+      "speciality": {
+        "code": "91020"
+      },
+      "taxIdentificationNumber": "223500420",
+      "taxIdQualifier": "",
+      "providerNetworks": {
+        "networkID": "04771"
+      },
+      "providerIdentificationNumber": "4458179",
+      "nationalProviderId": "1205949468",
+      "nationalNetworkParticipation": "",
+      "providerTier": ""
+    }
+  ]
+}
+
+
+
+{
+  "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
   "zipCode": "91355",
   "benefitProductType": "Medical",
   "languageCode": "11",
