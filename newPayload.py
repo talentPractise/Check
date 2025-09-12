dc05e246c9a03769e333c322549286f64d8aac93
Talent <tcassistance103@gmail.com>
2025-09-12 19:36:08 +0530

    Update newPayload.py

--- newPayload.py
+++ newPayload.py
@@ -1,11 +1,11 @@
 {
   "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
-  "zipCode": "63385",
+  "zipCode": "35234",
   "benefitProductType": "Medical",
   "languageCode": "11",
   "service": {
-    "code": "99213",
-    "type": "CPT4"
+    "code": "749",
+    "type": "DRG"
     "description": "Adult Office visit Age 30-39",
     "supportingService": {
       "code": "470",
@@ -16,66 +16,23 @@
     },
     "diagnosisCode": "F33 40",
     "placeOfService": {
-      "code": "11"
+      "code": "21"
     }
   },
   "providerInfo": [
     {
-      "serviceLocation": "157566",
+      "serviceLocation": "403411",
       "providerType": "",
       "speciality": {
-        "code": "90401"
+        "code": ""
       },
-      "taxIdentificationNumber": "223500420",
+      "taxIdentificationNumber": "630715625",
       "taxIdQualifier": "",
       "providerNetworks": {
-        "networkID": "01975"
+        "networkID": "03034"
       },
-      "providerIdentificationNumber": "7234118",
-      "nationalProviderId": "1740201425",
-      "nationalNetworkParticipation": "",
-      "providerTier": ""
-    }
-  ]
-}
-
-
-
-{
-  "membershipId": "5~265642286+34+44+20250201+784461+AM+39",
-  "zipCode": "91355",
-  "benefitProductType": "Medical",
-  "languageCode": "11",
-  "service": {
-    "code": "90901",
-    "type": "CPT4"
-    "description": "Adult Office visit Age 30-39",
-    "supportingService": {
-      "code": "470",
-      "type": "DRG"
-    },
-    "modifier": {
-      "modifierCode": "E1"
-    },
-    "diagnosisCode": "F33 40",
-    "placeOfService": {
-      "code": "11"
-    }
-  },
-  "providerInfo": [
-    {
-      "serviceLocation": "1186157",
-      "providerType": "MT",
-      "speciality": {
-        "code": "91175"
-      },
-      "taxIdentificationNumber": "811314333",
-      "taxIdQualifier": "",
-      "providerNetworks": {
-        "networkID": "08232"
-      },
-      "providerIdentificationNumber": "8559662",
-      "nationalProviderId": "1396096111",
+      "providerIdentificationNumber": "5988599",
+      "nationalProviderId": "1174573901",
       "nationalNetworkParticipation": "",
       "providerTier": ""
     }
